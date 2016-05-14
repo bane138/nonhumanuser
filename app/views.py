32568d655a0f16bd7b0c251c8ed5ccf14f24c02e
bane138 <david.pitzel@gmail.com>
2016-05-14 14:18:42 -0700

    fix homepage links

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ def index(request):
 	article= Entry.objects.last()
 	library_item = Item.objects.last()
 	game = Game.objects.last()
+	game_group = GameGroup.objects.get(name=game.group)
 	context = {
 		"site": { 
 			"title": "Non Human User",
@@ -18,6 +19,7 @@ def index(request):
 		"story": story,
 		"article": article,
 		"library_item": library_item,
-		"game": game
+		"game": game,
+		"game_group": game_group,
 	}
 	return render(request, 'app/index.html', context)
