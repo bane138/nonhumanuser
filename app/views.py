c2766ee24008b8079ac3f78dde817384f4cfbf74
bane138 <david.pitzel@gmail.com>
2016-06-04 16:53:06 -0700

    adding sidebar'

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ import datetime
 from actual_play.models import GameGroup, Game, Player
 from blog.models import Blog, Entry, Category
 from library.models import Stack, Item
+from itertools import chain
 
 # Create your views here.
 def index(request):
@@ -11,12 +12,10 @@ def index(request):
 	library_item = Item.objects.last()
 	game = Game.objects.last()
 	game_group = GameGroup.objects.get(name=game.group)
-	"""
-	entry_recent = Entry.objects.all().order_by('date_created')[0:5]
-	library_recent = Item.objects.all().order_by('date_created')[0:5]
-	games_recent = Game.objects.all().order_by('date_created')[0:5]
-	items_recent = entry_recent + library_recent + games_recent
-	"""
+	entry_recent = Entry.objects.filter(active__exact=True)
+	library_recent = Item.objects.filter(active__exact=True)
+	games_recent = Game.objects.filter(active__exact=True)
+	items_recent = list(chain(entry_recent, library_recent, games_recent))
 	context = {
 		"site": { 
 			"title": "Non Human User",
@@ -27,6 +26,6 @@ def index(request):
 		"library_item": library_item,
 		"game": game,
 		"game_group": game_group,
-		#"items_recent": items_recent[0:5]
+		"items_recent": games_recent #items_recent[0:5]
 	}
 	return render(request, 'app/index.html', context)
