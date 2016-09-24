92c583af843ebdef6bb81b6e0618ff3a95a40bc6
bane138 <david.pitzel@gmail.com>
2016-09-24 11:03:43 -0700

    fix merge

--- app/views.py
+++ app/views.py
@@ -14,98 +14,6 @@ from app.models import UserProfile
 
 
 # Create your views here.
-<<<<<<< HEAD
-def index(request):
-	story = Entry.objects.filter(category=1).last()
-	article= Entry.objects.filter(category=2).last()
-	library_item = Item.objects.filter(active=True).last()
-	game = Game.objects.last()
-	game_group = GameGroup.objects.filter(name=game.group).first()
-	entry_recent = Entry.objects.filter(active=True).order_by('-created_date')
-	library_recent = Item.objects.filter(active=True).order_by('-created_date')
-	games_recent = Game.objects.filter(active=True).order_by('-created_date')
-	items_recent = list(chain(entry_recent, library_recent, games_recent))
-	entry_popular = Entry.objects.filter(active=True).order_by('-number_comments')
-	library_popular = Item.objects.filter(active=True).order_by('-number_comments')
-	games_popular = Game.objects.filter(active=True).order_by('-number_comments')
-	items_popular = list(chain(entry_popular, library_popular, games_popular))
-	links = get_main_links()
-
-	context = {
-		"site": { 
-			'title': 'NonHumanUser',
-			'description': 'Stories, articles resources and supplements for Call of Cthulhu and related genre games.',
-		},
-		'og_type': 'webpage',
-		'og_url': 'http://www.nonhumanuser.com',
-		'og_title': 'NonHumanUser',
-		'og_description': 'Stories, articles resources and supplements for Call of Cthulhu and related genre games.',
-		'og_image': 'http://www.nonhumanuser.com/images/logo.png',
-		'story': story,
-		'article': article,
-		'library_item': library_item,
-		'game': game,
-		'game_group': game_group,
-		'items_recent': items_recent[0:5],
-		'items_popular': items_popular[0:5],
-		'section': 'main',
-		'links': links,
-	}
-	return render(request, 'app/index.html', context)
-
-
-def register(request):
-	"""
-	Register a user for the site
-	"""
-	context = RequestContext(request)
-
-	# Boolean for weather the registration was successful or not
-	registered = False
-
-	# If POST we are handling form data
-	if request.method == 'POST':
-		# Grab information from the raw form
-		user_form = UserForm(data=request.POST)
-		profile_form = UserProfileForm(data=request.POST)
-
-		# If forms are valid
-		if user_form.is_valid() and profile_form.is_valid():
-			user = user_form.save()
-
-			# Hash password and save user
-			user.set_password(user.password)
-			user.save()
-
-			# Since we need to set the user attribute ourselves, we set commit=False.
-			# This delays saving the model until we're ready to avoid integrity problems.
-			profile = profile_form.save(commit=False)
-			profile.user = user
-
-			# Do we have a profile picture?
-			if 'avatar' in request.FILES:
-				profile.avatar = request.FILES['avatar']
-
-			profile.save()
-
-			registered = True
-
-		else:
-			print(user_form.errors, profile_form.errors)
-
-	else:
-		user_form = UserForm()
-		profile_form = UserProfileForm()
-
-	return render_to_response(
-		'app/register.html',
-		{'user_form': user_form, 'profile_form': profile_form, 
-		'registered': registered, 'section': {'title': 'Register'}},
-		context)
-
-
-def user_login(request):
-=======
 class IndexView(View):
 
 	def get(self, request, *args, **kwargs):
@@ -148,7 +56,6 @@ class IndexView(View):
 
 
 class ProfileView(UpdateView):
->>>>>>> at_work
 	"""
 	Display user profile
 	"""
