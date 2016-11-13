896d484db5a70b582b4c125af08e3601d68dcd26
bane138 <david.pitzel@gmail.com>
2016-11-13 07:04:35 -0800

    fix truncate text filter

--- app/templatetags/main_extras.py
+++ app/templatetags/main_extras.py
@@ -7,6 +7,7 @@ def smart_truncate_chars(value, max_length):
     if len(value) > max_length:
         # Limits the number of characters in value tp max_length (blunt cut)
         truncd_val = value[:max_length]
+
         # Check if the next upcoming character after the limit is not a space, in which case it might be a word continuing
         if value[max_length] != " ":
             # rfind will return the last index where matching the searched character, in this case we are looking for the last space
@@ -14,5 +15,7 @@ def smart_truncate_chars(value, max_length):
             truncd_val = truncd_val[:truncd_val.rfind(" ")]
 
             return  truncd_val + "..."
+        else:
+            return truncd_val + "..."
 
     return value
