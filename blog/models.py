0da11c1d5a56e2c7b17d2966271d4d86ffc8c0ff
David Pitzel <david.pitzel@gmail.com>
2021-12-01 12:01:18 -0800

    Fix reverse import change in new django

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
-from django.core.urlresolvers import reverse
+from django.urls import reverse
+#from django.core.urlresolvers import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
