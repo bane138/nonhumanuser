1c2e887a29289104168d70b0064f7791a22f593d
David Pitzel <david.pitzel@gmail.com>
2025-08-28 07:25:55 -0700

    Update code to work with updated python and django versions

--- nonhumanuser/urls.py
+++ nonhumanuser/urls.py
@@ -5,15 +5,15 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import path, include
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.urls import include, path, re_path
 from django.contrib import admin
 from django.views.static import serve
 from app.views import IndexView, ProfileView, SearchView
@@ -23,33 +23,33 @@ ArticlesCommentView, StoriesCommentView
 from nonhumanuser import settings
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls')),
-    url(r'^stories/$', StoriesView.as_view(), name="stories"),
-    url(r'^stories/story_archive/$', StoryArchiveView.as_view(), name="story-archive"),
-    url(r'^stories/(?P<slug>[\w-]+)/$', StoryView.as_view(), name="story"),
-    url(r'^stories/(?P<slug>[\w-]+)/comment/$', StoriesCommentView.as_view(), 
+    path('', IndexView.as_view(), name='index'),
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('stories/', StoriesView.as_view(), name="stories"),
+    path('stories/story_archive/', StoryArchiveView.as_view(), name="story-archive"),
+    re_path(r'^stories/(?P<slug>[\w-]+)/$', StoryView.as_view(), name="story"),
+    re_path(r'^stories/(?P<slug>[\w-]+)/comment/$', StoriesCommentView.as_view(), 
         name='story comment'),
-    url(r'^articles/$', ArticlesView.as_view(), name="articles"),
-    url(r'^articles/article_archive/$', ArticleArchiveView.as_view(), name="article-archive"),
-    url(r'^articles/(?P<slug>[\w-]+)/$', ArticleView.as_view(), name="article"),
-    url(r'^articles/(?P<slug>[\w-]+)/comment/$', ArticlesCommentView.as_view(), 
+    path('articles/', ArticlesView.as_view(), name="articles"),
+    path('articles/article_archive/', ArticleArchiveView.as_view(), name="article-archive"),
+    re_path(r'^articles/(?P<slug>[\w-]+)/$', ArticleView.as_view(), name="article"),
+    re_path(r'^articles/(?P<slug>[\w-]+)/comment/$', ArticlesCommentView.as_view(), 
         name='article comment'),
-    url(r'^media/library/', include('library.urls')),
-    url(r'^library/', include('library.urls')),
-    url(r'^media/actual_play/', include('actual_play.urls')),
-    url(r'^actual_play/', include('actual_play.urls')),
-    url(r'^members/', include('django.contrib.auth.urls')),
-    url('accounts/', include('django_registration.backends.one_step.urls')),
-    url(r'^accounts/profile/(?P<slug>[\-\w]+)/$', ProfileView.as_view(), 
+    path('media/library/', include('library.urls')),
+    path('library/', include('library.urls')),
+    path('media/actual_play/', include('actual_play.urls')),
+    path('actual_play/', include('actual_play.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    re_path(r'^accounts/profile/(?P<slug>[\-\w]+)/$', ProfileView.as_view(), 
         name='update_user'),
-    url(r'^search/(?P<q>.*)$', SearchView.as_view(), name='search'),
+    re_path(r'^search/(?P<q>.*)$', SearchView.as_view(), name='search'),
 ]
 
 if settings.DEBUG:
     urlpatterns += [
-        url(r'^media/(?P<path>.*)$', serve, {
+        re_path(r'^media/(?P<path>.*)$', serve, {
             'document_root': settings.MEDIA_ROOT
             }),
         ]
