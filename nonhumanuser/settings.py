e3cab26b23ed573ff414c8f53ca457a95d41e67e
David Pitzel <david.pitzel@gmail.com>
2025-10-16 10:54:41 -0700

    Add dotenv package to use .env file for secrets

--- nonhumanuser/settings.py
+++ nonhumanuser/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
+from dotenv import load_dotenv
 import os
 #import dj_database_url
+load_dotenv()  # Load environment variables from .env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
